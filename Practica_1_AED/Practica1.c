099314c859d4d2da48381995daf710b29ad2be52
Zavaleta Guerrero Joshua Ivan <118492689+Zavjosh05@users.noreply.github.com>
2024-05-27 23:12:52 -0600

    P1:todas las funciones son funcionales
    arreglo en utilizacion de archivos

--- Practica_1_AED/Practica1.c
+++ Practica_1_AED/Practica1.c
@@ -1,10 +1,13 @@
+//Practica 1
+//2BM2 Zavaleta Guerrero Joshua Ivan
+//Compilarse junto a files.c
 #include<stdio.h>
-#include<stdlib.h>
 #include"files.h"
 
+//Ingresar el nombre del archivo al ejecutar el programa en la terminal
 int main(int argc, char *argv[])
 {
-	sortFile(argv);	
+	sortFile(++argv);	
 	
 	return 0;
 }
