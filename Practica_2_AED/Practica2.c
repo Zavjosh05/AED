c8f196abd32d9ea283bbc1d764cd0c076986e334
Zavaleta Guerrero Joshua Ivan <118492689+Zavjosh05@users.noreply.github.com>
2024-06-25 21:13:11 -0600

    P2: terminada
    Implementacion de reinas estaticas

--- Practica_2_AED/Practica2.c
+++ Practica_2_AED/Practica2.c
@@ -13,18 +13,23 @@ void fillZero(queen*);
 void printChessboardNums(queen);
 void printChessboard(queen);
 void printChessScreen(queen);
+void positioning(queen*,int,int);
 void queenPositioning(queen*,int,int);
-void eliminateQueen(queen*,int,int);
+void staticQueenPositioning(queen*,int,int);
 int checkQueenPositioning(queen,int,int);
 void queenElimCheck(queen*);
+void eliminateQueen(queen*,int,int);
 void ifExistElimQueen(queen*,int);
 int rowAvailability(queen,int);
 int queenPositionAvaliability(queen*,int,int);
 int queenCount(queen);
 int queensProblem(queen*,int);
+int staticQueensProblem(queen*,int);
+void staticQueens(queen*);
 void queenMenu(queen*);
 void queenInic();
 void staticQueens(queen*);
+void presentacion(int);
 
 int main(void)
 {
@@ -38,8 +43,16 @@ int chessDimension(){
 	int n;
 
 	printf("Insert chessboard dimension\n");
+	fseek(stdin,0,SEEK_END);
 	scanf("%d",&n);
-	printf("The chessboard dimensions are %d x %d\n\n",n,n);
+	if(n > 0)
+		printf("The chessboard dimensions are %d x %d\n\n",n,n);
+	else 
+	{
+		system("cls");
+		puts("\nIngrese una dimension valida");
+		return chessDimension();
+	}
 
 	return n;
 }
@@ -126,7 +139,46 @@ void printChessScreen(queen q)
 
 	printf("chessboard %d x %d\n",q.n,q.n);
 
+	//printChessboardNums(q);
 	printChessboard(q);
+}
+
+void positioning(queen *q,int m, int n)
+{
+	int i,j;
+
+	//cambios en columna
+	for(i = m-1; i >= 0; i--)
+		if(q->chessboard[i][n] == 0)
+			q->chessboard[i][n] = 1;
+	for(i = m+1; i < q->n; i++)
+		if(q->chessboard[i][n] == 0)
+			q->chessboard[i][n] = 1;
+	//cambios en fila
+	for(i = n-1; i >= 0; i--)
+		if(q->chessboard[m][i] == 0)
+			q->chessboard[m][i] = 1;
+	for(i = n+1; i < q->n; i++)
+		if(q->chessboard[m][i] == 0)
+			q->chessboard[m][i] = 1;
+	//cambios en diagonal
+	//ij
+	//++
+	for(i = m+1, j = n+1; i < q->n && j < q->n; i++,j++)
+		if(q->chessboard[i][j] == 0)
+			q->chessboard[i][j] = 1;
+	//+-
+	for(i = m+1, j = n-1; i < q->n && j >= 0; i++, j--)
+		if(q->chessboard[i][j] == 0)
+			q->chessboard[i][j] = 1;
+	//--
+	for(i = m-1, j = n-1; i >= 0 && j >= 0; i--, j--)
+		if(q->chessboard[i][j] == 0)
+			q->chessboard[i][j] = 1;
+	//-+
+	for(i = m-1, j = n+1; i >= 0 && j < q->n; i--, j++)
+		if(q->chessboard[i][j] == 0)
+			q->chessboard[i][j] = 1;
 }
 
 void queenPositioning(queen *q, int m, int n)
@@ -135,38 +187,7 @@ void queenPositioning(queen *q, int m, int n)
 
 	q->chessboard[m][n] = 2;
 
-	//cambios en columna
-	for(i = m-1; i >= 0; i--)
-		if(q->chessboard[i][n] == 0)
-			q->chessboard[i][n] = 1;
-	for(i = m+1; i < q->n; i++)
-		if(q->chessboard[i][n] == 0)
-			q->chessboard[i][n] = 1;
-	//cambios en fila
-	for(i = n-1; i >= 0; i--)
-		if(q->chessboard[m][i] == 0)
-			q->chessboard[m][i] = 1;
-	for(i = n+1; i < q->n; i++)
-		if(q->chessboard[m][i] == 0)
-			q->chessboard[m][i] = 1;
-	//cambios en diagonal
-	//ij
-	//++
-	for(i = m+1, j = n+1; i < q->n && j < q->n; i++,j++)
-		if(q->chessboard[i][j] == 0)
-			q->chessboard[i][j] = 1;
-	//+-
-	for(i = m+1, j = n-1; i < q->n && j >= 0; i++, j--)
-		if(q->chessboard[i][j] == 0)
-			q->chessboard[i][j] = 1;
-	//--
-	for(i = m-1, j = n-1; i >= 0 && j >= 0; i--, j--)
-		if(q->chessboard[i][j] == 0)
-			q->chessboard[i][j] = 1;
-	//-+
-	for(i = m-1, j = n+1; i >= 0 && j < q->n; i--, j++)
-		if(q->chessboard[i][j] == 0)
-			q->chessboard[i][j] = 1;
+	positioning(q,m,n);
 }
 
 void staticQueenPositioning(queen *q, int m, int n)
@@ -175,38 +196,7 @@ void staticQueenPositioning(queen *q, int m, int n)
 
 	q->chessboard[m][n] = 3;
 
-	//cambios en columna
-	for(i = m-1; i >= 0; i--)
-		if(q->chessboard[i][n] == 0)
-			q->chessboard[i][n] = 1;
-	for(i = m+1; i < q->n; i++)
-		if(q->chessboard[i][n] == 0)
-			q->chessboard[i][n] = 1;
-	//cambios en fila
-	for(i = n-1; i >= 0; i--)
-		if(q->chessboard[m][i] == 0)
-			q->chessboard[m][i] = 1;
-	for(i = n+1; i < q->n; i++)
-		if(q->chessboard[m][i] == 0)
-			q->chessboard[m][i] = 1;
-	//cambios en diagonal
-	//ij
-	//++
-	for(i = m+1, j = n+1; i < q->n && j < q->n; i++,j++)
-		if(q->chessboard[i][j] == 0)
-			q->chessboard[i][j] = 1;
-	//+-
-	for(i = m+1, j = n-1; i < q->n && j >= 0; i++, j--)
-		if(q->chessboard[i][j] == 0)
-			q->chessboard[i][j] = 1;
-	//--
-	for(i = m-1, j = n-1; i >= 0 && j >= 0; i--, j--)
-		if(q->chessboard[i][j] == 0)
-			q->chessboard[i][j] = 1;
-	//-+
-	for(i = m-1, j = n+1; i >= 0 && j < q->n; i--, j++)
-		if(q->chessboard[i][j] == 0)
-			q->chessboard[i][j] = 1;
+	positioning(q,m,n);
 }
 
 int checkQueenPositioning(queen q, int m, int n)
@@ -310,7 +300,6 @@ int queenCount(queen q)
 int queensProblem(queen *q, int lvl)
 {
 	int i,j, count = 0, qcount;
-
 	
 	ifExistElimQueen(q,lvl);
 
@@ -338,25 +327,77 @@ int queensProblem(queen *q, int lvl)
 	}
 }
 
+int staticQueensProblem(queen *q, int lvl)
+{
+	int i,j, count = 0, qcount, indicator = 0;
+
+	for(i = 0; i < q->n; i++)
+		if(q->chessboard[lvl][i] == 3)
+			indicator = 1;
+	
+	if(indicator)
+	{
+		if(lvl == q->n - 1)
+			return 1;
+		else
+			count += staticQueensProblem(q,lvl+1);
+	}
+	else
+	{
+		ifExistElimQueen(q,lvl);
+
+		if(lvl == q->n - 1)
+		{
+			qcount = rowAvailability(*q,lvl);
+			if(qcount){
+				queenPositioning(q,lvl,qcount-1);
+				printChessScreen(*q);
+				//system("pause");
+				eliminateQueen(q,lvl,qcount-1);
+				return 1;
+			}
+		}
+		else 
+		{
+			for(i = 0; i < q->n; i++)
+				if(q->chessboard[lvl][i] == 0){
+					queenPositioning(q,lvl,i);
+					printChessScreen(*q);
+					count += staticQueensProblem(q,lvl+1);
+					eliminateQueen(q,lvl,i);
+				}
+			return count;
+		}
+	}
+
+	
+}
+
 void staticQueens(queen *q)
 {
-	int qcount = 0,wait = 1, x, y;
+	int qcount = 0,wait = 1, y, xx;
+	char x;
 	char op;
 
 	while(wait)
 	{
 		fseek(stdin,0,SEEK_END);
-		system("cls");
+		//system("cls");
 		printChessboard(*q);
 		printf("\nStatic queens = %d\n",qcount);
-		printf("Insert the coordinates of the static queen (row, col): \n");
-		scanf("%d%d",&x,&y);
-		if((x >= 0 || y >= 0)&&(x <= q->n || y <= q->n))
+		printf("Insert the coordinates of the static queen (col, row): \n");
+		scanf("%c%d",&x,&y);
+		if(x >= 'A' && x <= 65+q->n)
+			xx = x-64;
+		if(x >= 'a' && x <= 97+q->n)
+			xx = x-96;
+		if((xx > 0 && y >= 1)&&(xx <= q->n && y <= q->n))
 		{
-			if(queenPositionAvaliability(q,x-1,y-1))
+			if(queenPositionAvaliability(q,y-1,xx-1))
 			{
-				staticQueenPositioning(q,x-1,y-1);
+				staticQueenPositioning(q,y-1,xx-1);
 				printChessboard(*q);
+				putchar('\n');
 				printf("\nStatic queens = %d\n",++qcount);
 				puts("Add another static queen? (y/n)");
 				fseek(stdin,0,SEEK_END);
@@ -365,15 +406,21 @@ void staticQueens(queen *q)
 					wait = 0;
 			}
 			else
-				puts("Invalid coordinates");
+			{
+				system("cls");
+				puts("Coordenadas no disponibles");
+			}
 			
 		}
 		else
+		{
+			system("cls");
 			printf("Ingrese coordenadas validas");
+		}
 	}
-}
 
-//int staticQueensProblem(queen *q, int lvl)
+	printf("Total of combinatios of an %dx%d chessboard = %d\n",q->n,q->n,staticQueensProblem(q,0));
+}
 
 void queenMenu(queen *q)
 {
@@ -388,7 +435,7 @@ void queenMenu(queen *q)
 	switch(x)
 	{
 		case '1':
-			printf("Total of combinatios of a %dx%d chessboard = %d\n",q->n,q->n,queensProblem(q,0));
+			printf("Total of combinatios of an %dx%d chessboard = %d\n",q->n,q->n,queensProblem(q,0));
 			break;
 		case '2':
 				staticQueens(q);
@@ -406,7 +453,8 @@ void queenInic()
 	char x;
 	int n;
 
-	system("cls");
+	presentacion(2);
+	putchar('\n');
 	puts("Queen's problem");
 
 	q.n = chessDimension();
@@ -416,4 +464,11 @@ void queenInic()
 	queenMenu(&q);
 
 	freeQueen(&q);
+}
+
+void presentacion(int n)
+{
+    system("cls");
+    puts("Zavaleta Guerrero Joshua Ivan\nBoleta = 2024630163\nGrupo: 2BM2");
+    printf("Practica No. %d\n\n", n);
 }
